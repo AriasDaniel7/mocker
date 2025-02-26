d9280843ed2c3898f84ccd1920523b893e27b600
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-02-26 17:36:28 -0500

    table of schema 🛠️

--- src/app/scheme/pages/home/home.component.ts
+++ src/app/scheme/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '../../../utilities/components/button/button.component';
 import { SearchComponent } from '../../../utilities/components/search/search.component';
+import { TableComponent } from '../../components/table/table.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, ButtonComponent, SearchComponent],
+  imports: [RouterModule, ButtonComponent, SearchComponent, TableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
