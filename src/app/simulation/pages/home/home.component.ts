9366d0f146983f8ed2c313e7b09d1eb71155618a
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-02-26 19:13:34 -0500

    design of finished simulations 🛠️

--- src/app/simulation/pages/home/home.component.ts
+++ src/app/simulation/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from '../../../utilities/components/search/search.component';
+import { TableComponent } from '../../components/table/table.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SearchComponent],
+  imports: [SearchComponent, TableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
